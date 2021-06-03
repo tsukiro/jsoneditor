b51ca68f17e8e122be5a4a2be51b4436f26d20ec
Bastian Vergara <bastian@rasen.cl>
2021-06-03 12:08:22 -0400

    Updating code with new component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,21 @@
 import './App.css';
-import { JsonEditor as Editor } from 'jsoneditor-react';
-import 'jsoneditor-react/es/editor.min.css';
 import React, { useState } from 'react';
 import { Upload } from './components/Upload/Upload';
+import { Editor } from './components/Editor/Editor';
 
 function App() {
 
-  const [descriptions, setDescription] = useState([]);
-  const downloadFile = async () => {
-    const fileName = "file";
-    const json = JSON.stringify(descriptions);
-    const blob = new Blob([json],{type:'application/json'});
-    const href = await URL.createObjectURL(blob);
-    const link = document.createElement('a');
-    link.href = href;
-    link.download = fileName + ".json";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  }
-  const handleChange = (data) => {setDescription(data); };
+  const [descriptions, setDescription] = useState("");
+
   if (descriptions && descriptions !== ""){   
     return (
       <div className="App">
-          <Editor
-            value={descriptions}
-            onChange={handleChange}
-          />
-          <button
-            onClick={() => downloadFile()}>{"Descargar JSON"}
-          </button>
+          <Editor incomingDescriptions={descriptions} />
       </div>
     );
   }else{
-    return (<Upload onUploadFile={(data) => setDescription(data) } />)
+    return (
+      <div className="App"><Upload onUploadFile={(data) => setDescription(data) } /></div>)
   }
 }
 
